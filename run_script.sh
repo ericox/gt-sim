f294e6b130cbd9c5616e6328cbba623dcce9461d
Eric Cox <emcox84@gmail.com>
2017-05-01 12:15:27 -0400

    removing prints

--- run_script.sh
+++ run_script.sh
@@ -2,9 +2,9 @@
 set -x
 for i in `seq 1 100`;
 do
-  n=15
+  n=4
   first='p'
   second='c'
-  python -m gtsim.sequence -n $n -f $first -s $second -b 2 -r True >> seq_${n}_${first}_${second}.csv 2>&1
+  python -m gtsim.sequence -n $n -f $first -s $second -b 10 -r True >> seq_${n}_${first}_${second}_b10.csv 2>&1
 done
 set +x
